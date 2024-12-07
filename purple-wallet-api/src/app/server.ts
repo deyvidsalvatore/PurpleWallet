24e4ec81e59a9f9d7a3fc587e7100c79cd1a5507
Deyvid Santos <deyvidsantosdasilva2002@gmail.com>
2024-12-07 16:18:28 -0300

    feat: Server TS Changes

--- purple-wallet-api/src/app/server.ts
+++ purple-wallet-api/src/app/server.ts
@@ -1,8 +1,11 @@
 import express, { Express, Request, Response } from "express";
 import { AuthRoutes } from "./routes/auth.routes";
 import bodyParser from "body-parser";
+import { connectDB } from "./config/database";
 
 const app: Express = express();
+
+connectDB();
 
 app.use(bodyParser.json());
 
@@ -11,5 +14,7 @@ const authRoutes = new AuthRoutes();
 app.get("/", (req: Request, res: Response) => {
   res.send({ welcome: "Hello, Welcome to Purple Wallet API" });
 });
+
 app.use("/auth", authRoutes.router);
+
 export default app;
