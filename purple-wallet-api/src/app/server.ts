770a3319a486961cb3a55f64d5ece534d5d7c5bc
Deyvid Santos <deyvidsantosdasilva2002@gmail.com>
2024-12-07 21:41:40 -0300

    feat: CORs configuration

--- purple-wallet-api/src/app/server.ts
+++ purple-wallet-api/src/app/server.ts
@@ -1,8 +1,13 @@
 import express, { Express, Request, Response } from "express";
+import cors from "cors";
 import { AuthRoutes } from "./routes/auth.routes";
 import bodyParser from "body-parser";
 import { connectDB } from "./config/database";
 import { TransactionRoutes } from "./routes/transaction.routes";
+
+const FRONTEND_ORIGINS = process.env.FRONTEND_ORIGINS
+  ? process.env.FRONTEND_ORIGINS.split(",")
+  : ["*"];
 
 const app: Express = express();
 
@@ -16,6 +21,14 @@ const transactionRoutes = new TransactionRoutes();
 app.get("/", (req: Request, res: Response) => {
   res.send({ welcome: "Hello, Welcome to Purple Wallet API" });
 });
+
+app.use(
+  cors({
+    origin: FRONTEND_ORIGINS,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use("/auth", authRoutes.router);
 app.use("/transactions", transactionRoutes.router);
